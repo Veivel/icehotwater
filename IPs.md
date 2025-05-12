df3e086711aecb43ed6f6b2e2fac925ded1b3c3f
Veivel <25278964+Veivel@users.noreply.github.com>
2025-05-12 12:16:14 +0700

    feat: update image versions, update httproute for gateway

--- IPs.md
+++
@@ -1,5 +0,0 @@
-## IP addresses
-
-- master: 34.232.30.223 (t2.medium)
-- worker1: 34.204.27.199 (t2.small)
-- worker2: 67.202.37.232
--- microservices/push.sh
+++ microservices/push.sh
@@ -1,5 +1,5 @@
-docker push veivel/auth-service:1.0
-docker push veivel/order-service:1.0
-docker push veivel/product-service:1.0
-docker push veivel/tenant-service:1.0
-docker push veivel/wishlist-service:1.0
+docker push veivel/auth-service:1.1
+docker push veivel/order-service:1.1
+docker push veivel/product-service:1.1
+docker push veivel/tenant-service:1.1
+docker push veivel/wishlist-service:1.1
